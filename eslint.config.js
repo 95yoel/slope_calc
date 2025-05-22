67e454c5998fea8a5885500ac5a5d8afb9becdeb
Yoel <villayoel@gmail.com>
2025-05-23 00:00:09 +0200

    -Update

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,28 @@
-import js from "@eslint/js"
 import globals from "globals"
-import tseslint from "typescript-eslint"
+import parser from "@typescript-eslint/parser"
+import pluginTs from "@typescript-eslint/eslint-plugin"
 import { defineConfig } from "eslint/config"
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
   {
-    files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser },
-
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": pluginTs
+    },
     rules: {
-      'no-console': ['warn', { allow: ['warn', 'error', 'info', 'group', 'groupEnd', 'debug' ,'groupCollapsed'] }],
-      'semi': ['error', 'never'],
-      '@typescript-eslint/no-unused-vars': ['warn'],
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      
+      ...pluginTs.configs.recommended.rules,
+      "no-console": ["warn", { allow: ["warn", "error", "info", "debug", "group", "groupEnd", "groupCollapsed"] }],
+      "semi": ["error", "never"],
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "@typescript-eslint/explicit-function-return-type": "off"
     }
-  },
-  tseslint.configs.recommended,
+  }
 ])
